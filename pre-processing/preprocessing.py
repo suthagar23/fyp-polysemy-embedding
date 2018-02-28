1e14be828c553e0729da4e388aa480299bc1ce55
suthagar23 <suthagar23@gmail.com>
2018-02-28 12:40:54 +0530

    multi threading changes

--- pre-processing/preprocessing.py
+++ pre-processing/preprocessing.py
@@ -6,30 +6,40 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-import multiprocessing as mp
-import random
-import string
+from nltk.corpus.reader.wordnet import WordNetError
+# import multiprocessing as mp
+from threading import Thread
 
+from printer import  Printer
 
-basePath = "/home/suthagar/Desktop/tmp/"
+# basePath = "/home/suthagar/Desktop/tmp/1billion/"
+basePath = "/media/suthagar/Data/Corpus/1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/"
+
 outputPath = basePath + "output/"
 lemmatizer = WordNetLemmatizer()
+linePrinter = Printer()
+cachedStopWords = stopwords.words("english")
+
 
 def runCodeForLine(line,index):
+    err=0
     outLine = {}
     outLine[index]= {}
     outLine[index]['actualLine'] = line
-    # wordsInLine = []
     sentencesInLine = sent_tokenize(line)
     outLine[index]['sentTokenize'] = sentencesInLine
-    outLine[index]['words'] = {}
+    outLine[index]['sentence'] = {}
+    sentCounter = 0
+    sentenceOut = {}
     for sentence in sentencesInLine:
+
+        sentenceCounter = str(sentCounter)
+        sentenceOut[sentenceCounter] = {}
+        sentenceOut[sentenceCounter]['actual'] = sentence
+        sentenceOut[sentenceCounter]['words'] = {}
         words = (word_tokenize(sentence))
-        outLine[index]['words']['wordTokenize'] = words
-        # wordsInLine.append(words)
-        outLine[index]['words']['info'] = {}
-        cleanTokens = words[:]
-        # sr = stopwords.words('english')
+        sentenceOut[sentenceCounter]['words']['wordTokenize'] = words
+        sentenceOut[sentenceCounter]['words']['info'] = {}
         wordCounter = 0
         wordOut = {}
         for token in words:
@@ -38,7 +48,7 @@ def runCodeForLine(line,index):
 
             wordOut[strWordCounter] = {}
             wordOut[strWordCounter]['word'] = token
-            if token in stopwords.words('english'):
+            if token in cachedStopWords:
                 wordOut[strWordCounter]['isStopWord'] = True
             else:
                 wordOut[strWordCounter]['isStopWord'] = False
@@ -46,75 +56,98 @@ def runCodeForLine(line,index):
                 # find Synonyms,Antonyms from WordNet
                 synonyms = []
                 antonyms = []
-                for syn in wordnet.synsets(token):
-                    for lemma in syn.lemmas():
-                        synonyms.append(lemma.name())
-                        if lemma.antonyms():
-                            antonyms.append(lemma.antonyms()[0].name())
-                synonyms = list(set(synonyms))
-                antonyms = list(set(antonyms))
-                wordOut[strWordCounter]['synonymsCount'] = len(synonyms)
-                wordOut[strWordCounter]['synonymsList'] = synonyms
-                wordOut[strWordCounter]['antonymsCount'] = len(antonyms)
-                wordOut[strWordCounter]['antonymsList'] = antonyms
+                try:
+                    for syn in wordnet.synsets(str(token)):
+                        for lemma in syn.lemmas():
+                            synonyms.append(lemma.name())
+                            if lemma.antonyms():
+                                antonyms.append(lemma.antonyms()[0].name())
+                    synonyms = list(set(synonyms))
+                    antonyms = list(set(antonyms))
+                    wordOut[strWordCounter]['synonymsCount'] = len(synonyms)
+                    wordOut[strWordCounter]['synonymsList'] = synonyms
+                    wordOut[strWordCounter]['antonymsCount'] = len(antonyms)
+                    wordOut[strWordCounter]['antonymsList'] = antonyms
 
-                # word Lemmatization
-                wordOut[strWordCounter]['lemmatize'] = {}
-                wordOut[strWordCounter]['lemmatize']['verb'] = lemmatizer.lemmatize(token, pos="v")
-                wordOut[strWordCounter]['lemmatize']['noun'] = lemmatizer.lemmatize(token, pos="n")
-                wordOut[strWordCounter]['lemmatize']['adjective'] = lemmatizer.lemmatize(token, pos="a")
-                wordOut[strWordCounter]['lemmatize']['adverb'] = lemmatizer.lemmatize(token, pos="r")
+                    # word Lemmatization
+                    wordOut[strWordCounter]['lemmatize'] = {}
+                    wordOut[strWordCounter]['lemmatize']['verb'] = lemmatizer.lemmatize(token, pos="v")
+                    wordOut[strWordCounter]['lemmatize']['noun'] = lemmatizer.lemmatize(token, pos="n")
+                    wordOut[strWordCounter]['lemmatize']['adjective'] = lemmatizer.lemmatize(token, pos="a")
+                    wordOut[strWordCounter]['lemmatize']['adverb'] = lemmatizer.lemmatize(token, pos="r")
+                except WordNetError as e:
+                    # print("WordNetError on concept {}".format(token))
+                    err+=1
+                except AttributeError as e:
+                    # print("Attribute error on concept {}: {}".format(token, e.message))
+                    err += 1
+                except:
+                    # print("Unexpected error on concept {}: {}".format(token, sys.exc_info()[0]))
+                    err += 1
+            sentenceOut[sentenceCounter]['words']['info'] = wordOut
 
-        outLine[index]['words']['info'] = wordOut
+        outLine[index]['sentence'] = sentenceOut
+        sentCounter+=1
     return outLine[index]
 
-def readByLines(processId, corpusLines, startLine, endLine, counter, isMultiProcessing):
+# processCompletedState=[]
+def readByLines(processId, corpusLines, startLine, endLine, counter, isMultiProcessing, totalLines):
     outLine = {}
-    print(" Process Id : ",processId,"( From line ",startLine," to ",endLine,")")
+    # print(" Process Id : ",processId,"( From line ",startLine," to ",endLine,")")
     for line in range(startLine, endLine):
+        if(totalLines != 1):
+            completedPercentage = int((counter - startLine)/(totalLines-1)*100)
+        else:
+            completedPercentage = 100
+        linePrinter.addValues(completedPercentage, processId)
         index = str(counter)
         counter += 1
         if(corpusLines[line]!="" or corpusLines[line].replace("\n")!=""):
             outLine[index] = runCodeForLine(corpusLines[line], index)
-
-    if(isMultiProcessing) :
-        output.put(outLine)
-    else:
-        return outLine;
+        output[index] = outLine[index]
 
 
-totalReadFilesCount = 2
-print("Process started : ", datetime.now())
+totalReadFilesCount = 10
+output ={}
+startTime = datetime.now()
+print("Process started : ", startTime)
 print("Total Files : ",totalReadFilesCount, end="\n")
 for i in range(0, totalReadFilesCount):
-    inputFileName =  "corpus-file-0" + str(i) + ".txt" # heldout-00005-of-00050
+    output = {}
+    counter = 0;
+    totalProcess = 1;
+    inputFileName =  "news.en.heldout-0000" + str(i) + "-of-00050" # heldout-00005-of-00050
     print("\nStarted File ", i, " - ", inputFileName)
     print("-------------------------------------------------")
-    outputFileName =  "corpus-file-0" + str(i) +"-output.txt"
+    outputFileName =  "corpus-file-0" + str(i) + "- PID" + str(totalProcess) + "-output.txt"
     print("Output File ", i, " - ", outputFileName)
     inputFile = open(basePath + inputFileName, "r")
     outputFile = open(outputPath + outputFileName, "w+")
 
-    counter=0;
-    outLine = {}
     corpusLines = inputFile.readlines()
     totalLines = len(corpusLines)
-    totalProcess = 4;
+    print(" Total Process : ", totalProcess)
+    if (totalLines < totalProcess):
+        print(" Total needed process : ", totalLines)
+        totalProcess = totalLines
+
     allocationForProcess = int(totalLines / totalProcess)
     lastAllocation = totalLines - allocationForProcess*totalProcess
     print(" Total Lines : ",totalLines)
-    print(" Total Process : ", totalProcess)
     print(" Allocation for a Process : ", allocationForProcess)
     print(" Last Allocation for a Process : ", lastAllocation)
 
+    wordnet.ensure_loaded()            # first access to wn transforms it
+
     if (allocationForProcess > 0):
-        output = mp.Queue()
-        # run it using process
-        processes = [mp.Process(target=readByLines, args=(processId,
+        linePrinter.initCompletedstate(totalProcess+1)
+        processes = [Thread(target=readByLines, args=(processId,
                                                           corpusLines,
                                                           processId * allocationForProcess,
                                                           (processId + 1) * allocationForProcess,
-                                                          processId * allocationForProcess, True)) for processId in range(totalProcess)]
+                                                          processId * allocationForProcess, True,allocationForProcess)) for processId in
+                     range(totalProcess)]
+
         # Run processes
         for p in processes:
             p.start()
@@ -122,28 +155,25 @@ for i in range(0, totalReadFilesCount):
         for p in processes:
             p.join()
 
-        # Get process results from the output queue
-        outLine = [output.get() for p in processes]
-
-
-
-    # run only last one
+    print("\n Running last process")
     processId = totalProcess # single process
-    outLine[totalProcess-1].update(readByLines(processId, corpusLines, processId * allocationForProcess,
-                                       processId * allocationForProcess + lastAllocation,
-                                       processId * allocationForProcess, False))
+    readByLines(processId, corpusLines, processId * allocationForProcess,
+                processId * allocationForProcess + lastAllocation,
+                processId * allocationForProcess, False, lastAllocation)
 
-    outputFile.write(json.dumps(outLine))
+
+    outputFile.write(json.dumps(output))
     inputFile.close()
     outputFile.close()
-    print("Output File : ", outputPath + outputFileName)
-print("\n\n Process Stoped : ", datetime.now())
+    print("\n\nOutput File : ", outputPath + outputFileName)
+
+endTime = datetime.now()
+print("\n\n Process Stopped : ", endTime)
+print("\n\n Duration : ", endTime - startTime)
 
 # Print the output
 # outputFileName = outputPath + "corpus-file-00-output.txt"
 # inputFile = open(outputFileName, "r")
 # for line in inputFile:
 #     print(line)
-
-
 
