59ca9bdc08ec8a2b89f6ad2fc9d1005b0ee9b87f
jsingam <j.jeyathan007@gmail.com>
2018-03-07 14:57:05 +0530

    Initial commit for the connect.This is to carry forwward for embedding

---
+++ connector/src/line sperator.py
@@ -0,0 +1,16 @@
+
+data=open("output.txt").read().decode('utf8')
+
+sentences=data.split(". ")
+
+result=""
+i=0;
+for text in sentences:
+    i+=1
+    print i
+    result+=(text+".\n")
+
+file=open("sentence.txt","w")
+file.write(result.encode('utf8'))
+file.close()
+
