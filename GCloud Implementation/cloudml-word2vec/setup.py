1bc26747be3008dd7804b923b9b2138989282c58
Suthagar23 <suthagar23@gmail.com>
2018-06-29 16:13:58 +0530

    Added GCloud implementation for word2vec

---
+++ GCloud Implementation/cloudml-word2vec/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['tensorflow==1.5', 'urllib3==1.22']
+
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='Word 2 vec',
+    requires=['word2vec_ops1.so']
+)
