77dc1ad2dccdf5631252a31b559add5969425ed8
piraveena <piraveena18@gmail.com>
2018-09-15 22:06:46 +0530

    changes in needed_vectors file

--- piraveena/needed-vectors.py
+++ piraveena/needed-vectors.py
@@ -1,27 +1,36 @@
 import json;
 basePath="data/"
-inputFile="vectors.txt"
-targetWordsFile="target-words.txt"
+inputFile="model/vectors.txt"
+targetWordsFile="top-500-polywords.txt"
 #Create a text file names as target-words.txt which has the list of words(with pos tag) that need to be clustered.
-outputfile="targetvectors"
+outputfile="top-500-vectors.txt"
 
 targetwords=set(open(basePath+targetWordsFile,"r").read().split("\n"));
+print (len(targetwords))
 
 words={}
 with open(basePath+inputFile) as vectors:
     for vector in vectors:
         vec=vector.split(" ")
         parts=vec[0].split("_")
-        if (len(parts)==3):
-            word="_".join([parts[0],parts[1]])
-            if(word in targetwords):
-                w=vec.pop(0)
-                if(word in words):
-                    words[word][w]=" ".join(vec)
-                else:
-                    dict={}
-                    dict[w]=" ".join(vec)
-                    words[word]=dict
+        # print (len(parts))
+        word = ""
+        for x in range(len(parts)):
+            # print(x)
+            if x == 0:
+                word = parts[x]
+            else:
+                word= word + "_" + parts[x]
+        # print ("In vectors: "+ word)
+        if(word in targetwords):
+            # print ("In target vectors: " + word)
+            w=vec.pop(0)
+            if(word in words):
+                words[word][w]=" ".join(vec)
+            else:
+                dict={}
+                dict[w]=" ".join(vec)
+                words[word]=dict
 
 results = json.dumps(words)
 
