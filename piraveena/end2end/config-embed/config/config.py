c1e7f918f54f31c9e1163cc116e729f0d95c1ec1
piraveena <piraveena18@gmail.com>
2018-09-24 17:06:50 +0530

    adding end2end

---
+++ piraveena/end2end/config-embed/config/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+aws_keys = {
+    'access_key_id' :'AKIAJQUESCYF4RJIR53Q',
+    'secret_access_key':'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',
+}
+
+bucket_name="ml-embedding"
